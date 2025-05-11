068f8ae51a43463a0560be175565a5ef3c884aaa
Justine Visitacion <Carl.justinev@gmail.com>
2025-05-12 00:30:48 +0200

    feat(all): fix gitignore

---
+++ reAnimate-playground/vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: 'dist',  // Output directory for the final build
+    rollupOptions: {
+      input: './index.html', // Points to the main HTML file
+    },
+  },
+})
