24c78553064b7d30d3289444251d9657eb67c0ad
Justine Visitacion <Carl.justinev@gmail.com>
2025-05-11 23:59:10 +0200

    feat(all): configs

--- reAnimate-playground/src/App.vue
+++ reAnimate-playground/src/App.vue
@@ -3,13 +3,28 @@
 </template>
 
 <script>
-import * as animations from '@re-code/animate';
+import { animate, easings } from '@re-code/animate';
 
 export default {
   name: 'App',
   mounted() {
-    console.log(animations);
+    const box = this.$refs.box;
 
+    const config = {
+      from: 0,
+      to: 300,
+      duration: 2000,
+      easing: easings().easeOutQuad,
+    };
+
+    animate(config).subscribe({
+      next: (state) => {
+        box.style.transform = `translateX(${state.value}px)`; // Move the box
+      },
+      complete: () => {
+        console.log('Animation complete!');
+      },
+    });
   },
 };
 </script>
