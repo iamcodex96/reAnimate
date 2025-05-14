ce35a3c9798c8cdb689cddb5e76418ed18087a4d
Justine Visitacion <Carl.justinev@gmail.com>
2025-05-14 18:41:41 +0200

    feat(test): CI deployment

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: './dist',
+  base: '/playground/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
