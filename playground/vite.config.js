42b43f22c231b586b90b213e2737a6c2a1f2f791
Justine Visitacion <Carl.justinev@gmail.com>
2025-05-14 18:47:51 +0200

    Update vite.config.js

--- playground/vite.config.js
+++ playground/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/reAnimate/',
+  base: '/reAnimate/playground/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
